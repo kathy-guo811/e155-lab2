3cf7084baecd4563d6e4ac2041cc9c35a65aca54
kguo <yiwen.k.guo@gmail.com>
2026-09-15 14:25:22 -0700

    added tb

---
+++ fpga/tb/lab2_kg_tb.sv
@@ -0,0 +1,80 @@
+`timescale 1ns/1ns
+
+module lab2_kg_tb;
+
+    logic [3:0] sl;
+    logic [3:0] sr;
+    logic rst;
+
+    logic [6:0] segment;
+    logic [1:0] anode;
+    logic [3:0] led;
+
+    lab2_kg #(
+        .counter_width(2),
+        .counter_max_count(3),
+        .scanner_width(2),
+        .scanner_max_count(3)
+    ) dut (
+        .sl(sl),
+        .sr(sr),
+        .rst(rst),
+        .segment(segment),
+        .anode(anode),
+        .led(led)
+    );
+
+    initial begin
+
+        rst = 0;
+
+        #20;
+
+        // test reset
+        if (led == 4'b1000)
+            $display("TOP RESET PASS");
+        else
+            $error("TOP RESET FAILED: led = %b", led);
+
+        // release reset
+        rst = 1;
+
+        // test led scanning
+        #50;
+        if (led == 4'b0100)
+            $display("LED SCAN 1000 -> 0100 PASS");
+        else
+            $error("LED SCAN 1000 -> 0100 FAILED: led = %b", led);
+
+        #50;
+        if (led == 4'b0010)
+            $display("LED SCAN 0100 -> 0010 PASS");
+        else
+            $error("LED SCAN 0100 -> 0010 FAILED: led = %b", led);
+
+        #50;
+        if (led == 4'b0001)
+            $display("LED SCAN 0010 -> 0001 PASS");
+        else
+            $error("LED SCAN 0010 -> 0001 FAILED: led = %b", led);
+
+        // test time mux
+        sl = 4'h3;
+        sr = 4'h5;
+
+        #20;
+        if (anode == 2'b10)
+            $display("ANODE 0 SELECTION PASS");
+        else
+            $error("ANODE 0 SELECTION FAILED: anode = %b", anode);
+
+        #20;
+        if (anode !== 2'b01)
+            $error("ANODE 1 SELECTION FAILED: anode = %b", anode);
+        else
+            $display("ANODE 1 SELECTION PASS");
+
+        $stop;
+    end
+
+endmodule
---
+++ fpga/tb/scanner_tb.sv
@@ -0,0 +1,91 @@
+`timescale 1ns/1ns
+
+module scanner_tb;
+
+    logic clk;
+    logic reset;
+    logic enable;
+    logic [3:0] row;
+
+    // Instantiate scanner with a very small counter
+    // so simulation runs quickly.
+    scanner #(
+        .width(2),
+        .max_count(3)
+    ) dut (
+        .clk(clk),
+        .reset(reset),
+        .enable(enable),
+        .row(row)
+    );
+
+    /// generate clock
+	always begin
+		clk = 0; #5;
+		clk = 1; #5;
+	end
+
+
+    initial begin
+        clk = 0;
+        reset = 0;
+        enable = 0;
+
+        #10;
+
+        // test reset
+        if (row == 4'b1000)
+            $display("RESET PASSED");
+        else
+            $display("RESET FAILED: row = %b", row);
+
+        // Enable scanner
+        reset = 1;
+        enable = 1;
+
+        // first transition
+        #40;
+
+        if (row == 4'b0100)
+            $display("TRANSITION 1000 -> 0100 PASS")
+        else
+            $error("TRANSITION 1000 -> 0100 FAILED: row = %b", row);
+
+        // second transition
+         #40;
+
+        if (row == 4'b0010)
+            $display("TRANSITION 0100 -> 0010 PASS")
+        else
+            $error("TRANSITION 0100 -> 0010 FAILED: row = %b", row);
+
+        // third transition
+        #40;
+
+        if (row == 4'b0001)
+            $display("TRANSITION 0010 -> 0001 PASS")
+        else
+            $error("TRANSITION 0010 -> 0001 FAILED: row = %b", row);
+
+        // fourth transition
+        #40;
+
+        if (row == 4'b1000)
+            $display("TRANSITION 0001 -> 1000 PASS")
+        else
+            $error("TRANSITION 0001 -> 1000 FAILED: row = %b", row);
+
+        // test enable
+        enable = 0;
+
+        #80;
+
+        if (row == 4'b1000)
+            $display("ENABLE PASS");
+        else
+            $error("ENABLE FAILED: row changed while disabled");
+
+        $stop;
+    end
+
+endmodule
