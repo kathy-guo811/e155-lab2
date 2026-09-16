b96e4d31d35775ea5a18b9c2a708af5ee36362dc
kguo <yiwen.k.guo@gmail.com>
2026-09-16 09:36:27 -0700

    updated counter to wrap blinker

--- fpga/src/counter.sv
+++ fpga/src/counter.sv
@@ -1,38 +1,23 @@
 // Kathy Guo
 // kaguo@g.hmc.edu
 // 9/14/2026
-// Counter that increments on each positive clock edge. 
-// Once it reaches max_count, counter it resets to zero and it switches 
-
+// Wraps blinker to produce digit_select, the multiplex toggle for the
+// dual seven-segment display.
 
 module counter #(
 	parameter width = 23,
-	parameter max_count = 24'd24000)
-   (input logic clk,
-	input logic reset,
-	input logic enable,
+	parameter max_count = 17'24000)
+   (input  logic clk,
+	input  logic reset,
+	input  logic enable,
 	output logic digit_select
 );
 
-	logic [width:0] count;
-
-	always_ff @(posedge clk) begin
-		if (reset == 0) begin
-			count <= 0;
-            digit_select <= 0;
-		end
-		else if (enable) begin
-			if (count == max_count) begin
-				count <= 0;
-                digit_select <= ~digit_select;
-			end
-			else begin
-				count <= count + 1'b1;
-			end
-		end
-		else begin
-			count <= count;
-		end
-	end
+	blinker #(.width(width), .max_count(max_count)) blink (
+		.clk(clk),
+		.reset(reset),
+		.enable(enable),
+		.led(digit_select)
+	);
 
 endmodule
