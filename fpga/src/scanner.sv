c7a983c22ecd5d199e3d5311779bb56aa58e379f
kguo <yiwen.k.guo@gmail.com>
2026-09-22 16:16:02 -0700

    updated scanner

--- fpga/src/scanner.sv
+++ fpga/src/scanner.sv
@@ -6,37 +6,31 @@
 
 module scanner #(
 	parameter width = 24,
-	parameter max_count = 24'd10000000)
+	parameter max_count = 24'd5000000)
 	(input  logic clk,
 	 input  logic reset,
-	 input  logic enable,
+   input  logic enable,
 	 output logic [3:0] row
 );
 
-	logic led;
+	logic count;
 	logic led_prev;
 
 	blinker #(.width(width), .max_count(max_count)) blink_s (
 		.clk(clk),
 		.reset(reset),
 		.enable(enable),
-		.led(led)
+		.count(count)
 	);
-	
+
 	always_ff @(posedge clk, negedge reset) begin
-		if (reset == 0) begin  				// active low reset
-			row <= 4'b1000;
-			led_prev <= 1'b0;
-		end
-		else begin
-			led_prev <= led; 				// update led_prev to equal led
-			if (led != led_prev) begin
-				if (row == 4'b0001)			// wrap around if in the last row
-					row <= 4'b1000;
-				else
-					row <= row >> 1'b1;		// bit shifting
-			end
+		if (reset != 1) begin 
+			if (count <= max_count/4 && count >= 0) row <= 4'b0001;
+			else if (count <= max_count/2 && count >= max_count/4) row <= 4'b0010;
+			else if (count <= 3*max_count/4 && count >= max_count/2) row <= 4'b0100;
+			else row <= 4'b1000;
 		end
 	end
+
 
 endmodule
